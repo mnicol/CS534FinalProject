be971e92076096395c6097fa96bf13e4ddb09dd8
Bradmiller72 <millerbradley72@gmail.com>
2016-12-17 23:22:52 -0600

    fixed some shit ya feel

--- main.m
+++ main.m
@@ -1,24 +1,20 @@
-function [ out ] = main( )
+function [ out ] = main( img1, img2, top, bottom, left, right, poissonPlacement )
 %MAIN Summary of this function goes here
 %   Detailed explanation goes here
-    img = imread('/Users/bradmiller/Documents/MATLAB/534_final/Fire-Hydrant-Installation-Schererville-IN-300x200.jpg');
+    %img = imread('/Users/bradmiller/Documents/MATLAB/534_final/Fire-Hydrant-Installation-Schererville-IN-300x200.jpg');
     %img = imread('tiger.jpg');
-    [m,n,k] = size(img);
-    backgroundBox = zeros(m,n);
-    pixel1 = {50,10};
-    pixel2 = {200,100};
-    pixel3 = {50,190};
-    pixel4 = {200,190};
+    [m,n,k] = size(img1);
     
-    for i=pixel1{1}:pixel2{1}
-       for j=pixel1{2}:pixel3{2}
+    backgroundBox = zeros(m,n,k);
+    for i=left:right
+       for j=top:bottom
            backgroundBox(j,i,:) = 1;
        end
     end
     
     JND = 3.6;
     
-    out = GraphCut2(img, backgroundBox, JND);
+    [img_cut, img_background] = kMeansCut(img1, backgroundBox, JND);
 
 end
 
