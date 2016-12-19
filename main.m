a46699c1474a22260edadbb632d944593a761778
James Merrill <jdmerrill15@gmail.com>
2016-12-18 20:48:23 -0600

    Target is now placed based on the upper left coordinates passed into main.m

--- main.m
+++ main.m
@@ -1,10 +1,14 @@
-function [ out ] = main( img1, img2, top, bottom, left, right, poissonPlacement )
+%img1 = source image
+%img2 = target image
+%top, bottom, left, right = boundaries of selected object
+%targetrow, targetcol = The coordinates of the upper left corner of the box
+%in our target image.
+function [ out ] = main( img1, img2, top, bottom, left, right, targetrow, targetcol )
 %MAIN Summary of this function goes here
 %   Detailed explanation goes here
     %img = imread('/Users/bradmiller/Documents/MATLAB/534_final/Fire-Hydrant-Installation-Schererville-IN-300x200.jpg');
     %img = imread('tiger.jpg');
     [m,n,k] = size(img1);
-    
     backgroundBox = zeros(m,n,k);
     for i=left:right
        for j=top:bottom
@@ -19,10 +23,10 @@ function [ out ] = main( img1, img2, top, bottom, left, right, poissonPlacement 
     fullmask = imbinarize(rgb2gray(img_cut));
     
     sourcesection = img1(top:bottom, left:right, :);
-    targetsection = img2(top:bottom, left:right, :);
+    targetsection = img2(top + targetrow:bottom + targetrow, left + targetcol:right + targetcol, :);
     masksection = fullmask(top:bottom, left:right);
     blendedsection = Blender(sourcesection, targetsection, masksection);
     out = im2double(img2);
-    out(top:bottom, left:right, :) = blendedsection;
+    out(top + targetrow:bottom + targetrow, left + targetcol:right + targetcol, :) = blendedsection;
 end
 
